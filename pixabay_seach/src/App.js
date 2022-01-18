8686834180cc17825ec79a415944ede7e0c60f4c
riataso <nkc20212452@denpa.jp>
2022-01-18 14:12:27 +0900

    first_commit

---
+++ pixabay_seach/src/App.js
@@ -0,0 +1,31 @@
+import React,{ useState } from "react";
+import axios from 'axios';
+import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+
+const App = () => {
+    const [images, setimages] =useState([]);
+    const onSearchSubmit =async (term) =>{
+        try {
+            const params={
+                key: "25051452-22dcb24d68c2ea224c7404fdf",
+                q  : term,
+            };
+            const response= await axios.get('https://pixabay.com/api/',{ params });
+            setimages(response.data.hits);
+            if(response.data.total===0){
+                alert('お探しの画像はありません');
+            }
+        } catch {
+            alert('写真の取得に失敗');
+        }
+    };
+    return (
+        <div className="ui container" style={{marginTop: "20px"}}>
+            <SearchBar onSubmit={onSearchSubmit} />
+            <ImageList images={images} />
+        </div>
+    );
+};
+
+export default App;
---
+++ pixabay_seach/src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const SearchBar=({onSubmit})=>{
+    const [term,setTerm]=useState('');
+    const onFormSabmit=(event)=>{
+        event.preventDefault();
+        onSubmit(term);
+    };
+    return(
+      <form onSubmit={onFormSabmit} className="ui form">
+        <div className="field">
+          <label>ImageSearch</label>
+          <input type="text" name="Search" placeholder="" value={term} onChange={(event)=>{
+              setTerm(event.target.value);//検索欄に入力された文字列
+          }}
+          />
+        </div>
+      </form>
+    );
+};
+
+export default SearchBar;
